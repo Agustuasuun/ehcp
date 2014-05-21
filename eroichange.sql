cd23035d1ea297a21ff3fb0e346254fbe2c74405
Jason Brooks <jason@eroi.com>
2014-05-21 14:31:20 -0700

    updated eroichange.sql

--- eroichange.sql
+++ eroichange.sql
@@ -3,6 +3,6 @@ use ehcp;
 insert into misc (name,value,longvalue,comment) values
 ('enablecommonwebstats', '', NULL,'webalizer common webstats'),
 ('commonwebstatsdir', '/net/workhorse/backup1/common-web/webstats', NULL, 'webalizer common directory'),
-('commonbackupconfdir','/net/workhorse/backup1/backupscripts/conf.d',NULL,'common backup config directory),
+('commonbackupconfdir','/net/workhorse/backup1/backupscripts/conf.d',NULL,'common backup config directory')
 
 
